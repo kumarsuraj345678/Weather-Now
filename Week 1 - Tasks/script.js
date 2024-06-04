4c25a91f50773c3952559c32685a52ff63a4c056
surajkumar345678 <kumarsuraj345678@gmail.com>
2024-06-04 13:28:59 +0530

    Refactored html, css and javascript files code. Added svg weather icons.

--- Week 1 - Tasks/script.js
+++ Week 1 - Tasks/script.js
@@ -11,31 +11,44 @@ async function checkWeather(city) {
   if (response.status == 404) {
     document.querySelector(".error").style.display = "block";
     document.querySelector(".weather").style.display = "none";
-} else {
+  } else {
     var data = await response.json();
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML =
       data.main.temp.toFixed(1) + "&deg;C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    document.querySelector(".desc").textContent = data.weather[0].description;
     document.querySelector(".feels_like").innerHTML =
       data.main.feels_like.toFixed(1) + "&deg;C";
 
-    if (data.weather[0].main == "Clouds") {
-      weatherIcon.src = "images/clouds.png";
-    } else if (data.weather[0].main == "Clear") {
-      weatherIcon.src = "images/clear.png";
-    } else if (data.weather[0].main == "Rain") {
-      weatherIcon.src = "images/rain.png";
+    if (data.weather[0].main == "Thunderstorm") {
+      weatherIcon.src = "images/thunderstorms.svg";
     } else if (data.weather[0].main == "Drizzle") {
-      weatherIcon.src = "images/drizzle.png";
+      weatherIcon.src = "images/drizzle.svg";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/rain.svg";
+    } else if (data.weather[0].main == "Snow") {
+      weatherIcon.src = "images/snow.svg";
     } else if (data.weather[0].main == "Mist") {
-      weatherIcon.src = "images/mist.png";
+      weatherIcon.src = "images/mist.svg";
+    } else if (data.weather[0].main == "Smoke") {
+      weatherIcon.src = "images/smoke.svg";
+    } else if (data.weather[0].main == "Haze") {
+      weatherIcon.src = "images/haze.svg";
+    } else if (data.weather[0].main == "Fog") {
+      weatherIcon.src = "images/fog.svg";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/clear-day.svg";
+    } else if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/cloudy.svg";
+    } else if (data.weather[0].main == "night") {
+      weatherIcon.src = "images/clear-night.svg";
     }
 
     document.querySelector(".error").style.display = "none";
     document.querySelector(".weather").style.display = "block";
-}
+  }
 }
 
 searchBtn.addEventListener("click", () => {
